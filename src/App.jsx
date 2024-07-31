12b5a492544651c2e2068237169b17662e819a2c
Amit <amit@Amits-MacBook-Pro.local>
2024-07-31 22:10:57 +0530

    first commit for TestMack Project. After configer SSH key in my mac

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+<main>
+  <section>
+    <h1>Jai BholaNath</h1>
+    <h2>Add Speed Test</h2>
+    <h3>Good Test Result till now</h3>
+    <h4>i am ok with this</h4>
+  </section>
+</main>
+  )
+}
+
+export default App
